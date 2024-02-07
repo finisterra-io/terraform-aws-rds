195673f2368a879695ab9615ee23e67b98f16e54
Daniel J <daniel@finisterra.io>
2024-02-06 21:49:25 -0500

    Update required versions of AWS provider

--- modules/db_instance/versions.tf
+++ modules/db_instance/versions.tf
@@ -1,15 +1,10 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.1"
+      version = ">= 4.67"
     }
   }
 }
